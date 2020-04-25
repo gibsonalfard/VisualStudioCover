c0a5b4aa071bcd0593c8e2ad404e9d9ea9e81b55
Ilham Gibran <ilgibrz@gmail.com>
2020-04-25 12:14:43 +0700

    Add Some Unit Test

--- VisualStudioCover/UnitTest4.cs
+++ VisualStudioCover/UnitTest4.cs
@@ -8,13 +8,47 @@ namespace VisualStudioCover
     {
         public Kombinasi combine = new Kombinasi();
         [TestMethod]
-        public void OutputBintang_42_Test1()
+        public void OutputBintang_42_Test()
         {
             // Karena yang akan diuji adalah Coverage
             // Untuk Void Function hanya perlu dipanggil
             // Tanpa Memberi nilai kembalian
 
+            // Masuk ke Else
+            combine.OutputBintang_42(0);
+            // Masuk ke If
+            combine.OutputBintang_42(2);
+        }
 
+        [TestMethod]
+        public void isBilPrima_43_Test()
+        {
+            // Path Coverage gak Full Harusnya terdapat 4 IndPath
+            // Saat dieksekusi 1 Path saja yang memenuhi
+            bool result;
+            /*
+            // Tidak masuk ke If (If bernilai False)
+            result = combine.isBilPrima_43(1);
+            Assert.AreEqual(result, false);
+            
+            // Masuk ke If (If bernilai True)
+            // Tidak masuk Loop
+            result = combine.isBilPrima_43(2);
+            Assert.AreEqual(result, false);
+            
+            // Masuk ke If (If bernilai True)
+            // Masuk Loop
+            // If false
+            result = combine.isBilPrima_43(3);
+            Assert.AreEqual(result, false);
+            */
+
+            // Masuk ke If (If bernilai True)
+            // Masuk Loop
+            // If true
+            result = combine.isBilPrima_43(4);
+            Assert.AreEqual(result, true);
+            
         }
     }
 }
