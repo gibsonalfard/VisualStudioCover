b7303fb118772cbd3179e83ea7eaee149b293062
Zikri Ariachandra <zikriariachandra@gmail.com>
2020-05-01 15:22:15 +0700

    Create UnitTest5.cs
    Membuat Unit Test untuk Faktorial

---
+++ VisualStudioCover/UnitTest5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VisualStudioCover
+{
+    [TestClass]
+    public class UnitTest5
+    {
+        public int intA, intB, intC, intD;
+        public float flA, flB, flC, flD;
+        public double dblA, dblB, dblC, dblD;
+        public char chA, chB, chC, chD;
+        public string strA, strB, strC, strD;
+        public Faktorial Fak = new Faktorial();
+/*==================================================================================*/
+        [TestMethod]
+        public void factorial_Test()
+        {
+            intA = -1;
+            int result = Fak.factorial(intA);
+            Assert.AreEqual(result, -1);
+        }
+
+        [TestMethod]
+        public void factorial_Test()
+        {
+            intA = 1;
+            int result = Fak.factorial(intA);
+            Assert.AreEqual(result, 1);
+        }
+/*==================================================================================*/
+    }
+}
